2e4cea784c661a6e42b76f6dd5be4c7eb31d1107
rahmd0012 <rahmd0012@wrdsb.ca>
2020-10-01 13:46:07 +0000

    Finished switch case

--- Main.java
+++ Main.java
@@ -14,6 +14,32 @@ public class Main {
     // Create a scanner for user input
     Scanner input = new Scanner(System.in);
 
+    // switch case example
+    // get the user choice between 1 and 5
+    System.out.println("Please make a selection (1-5)");
+
+    
+    int choice = input.nextInt();
+    // gets rid of the enter key so we can get strings
+    input.nextLine();
+
+    // determine which candy to drop
+    switch (choice){
+      case 1:
+        System.out.println ("Here is your chocolate bar");
+        break;
+      case 2:
+        System.out.println("Here are your Skittles");
+        break;
+      case 3: 
+        System.out.println("Here are your M&Ms");
+        break;
+      default:
+        System.out.println("Invalid Choice");
+    }
+    
+    
+    // nested loop example
     // ask the user if the animal has feathers
     System.out.println("Does the animal have feathers? (yes/no)");
     String featherAnswer = input.nextLine();
